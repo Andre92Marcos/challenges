73af0d5b72f6a62481f99c750cf5e296006a60fc
Andre Marcos <andre92marcos@gmail.com>
2021-05-21 11:16:02 -0400

    Finished int to string challenge

---
+++ hackerRank/java/introduction/int_to_string/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Solution{
+
+	public static void main(String[] args){
+
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		String s = Integer.toString(i);
+		System.out.println("Good Job");
+	}
+}
